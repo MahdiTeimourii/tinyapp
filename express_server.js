3911f05cd5d68af87758c383fcd88ee590c362ad
Mahdi Teimouri <mahditei@mail.com>
2023-05-16 15:57:53 -0700

    commit2

--- express_server.js
+++ express_server.js
@@ -1,14 +1,23 @@
+function generateRandomString() {
+
+};
+
 const express = require("express");
 const app = express();
+
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -19,6 +28,9 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: longURL };
