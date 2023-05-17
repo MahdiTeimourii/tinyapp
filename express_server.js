8f82d2cbb6861fc1616b3d6f0eb97058d6a3083d
Mahdi Teimouri <mahditei@mail.com>
2023-05-16 19:48:02 -0700

    we are getting there

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 function generateRandomString() {
 
-};
+}
 
 const express = require("express");
 const app = express();
@@ -12,54 +12,79 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.post("/urls", (req, res) => {
-  const id = generateRandomString(); // Generate a random ID
-  const longURL = req.body.longURL; // Extract the longURL from the request body
-  urlDatabase[id] = longURL; // Add the new key-value pair to the urlDatabase
-  res.redirect(`/urls/${id}`); // Redirect to /urls/:id
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  if (longURL) {
+    const templateVars = { id, longURL };
+    res.render("urls_show", templateVars);
+  } else {
+    res.status(404).send("URL not found");
+  }
+});
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newLongURL = req.body.longURL;
+
+  if (urlDatabase[id]) {
+    urlDatabase[id] = newLongURL;
+    res.redirect("/urls");
+  } else {
+    res.status(404).send("URL not found");
+  }
+});
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+
+  if (urlDatabase[id]) {
+    delete urlDatabase[id];
+    res.redirect("/urls");
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: longURL };
-  res.render("urls_show", templateVars);
-});
 
-app.get("/u/:id", (req, res) => {
-  const id = req.params.id; // Get the ID from the request parameters
-  const longURL = urlDatabase[id]; // Retrieve the long URL from the urlDatabase using the ID
-
-  if (longURL) {
-    res.redirect(longURL); // Redirect to the long URL
-  } else {
-    res.status(404).send("URL not found"); // Handle case when the ID doesn't exist in the urlDatabase
-  }
-});
-app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id; // Get the ID from the request parameters
-
-  if (urlDatabase[id]) {
-    delete urlDatabase[id]; // Use the delete operator to remove the URL
-    res.redirect("/urls"); // Redirect back to the urls_index page
-  } else {
-    res.status(404).send("URL not found"); // Handle case when the ID doesn't exist in the urlDatabase
-  }
-});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
