0c1b5f2129cd8702a4caa17fdf6e123b403de2b3
Mahdi Teimouri <mahditei@mail.com>
2023-05-18 20:12:04 -0700

    another commit

--- express_server.js
+++ express_server.js
@@ -10,10 +10,15 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const users = {};
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -26,29 +31,42 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: req.cookies["user_id"] };
   console.log(templateVars.user)
 
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { user: req.cookies["username"] };
-  res.render("urls_new", templateVars);
+  const user = req.cookies["user_id"];
+
+  if (!user) {
+    res.redirect("/login");
+  } else {
+    const templateVars = { user };
+    res.render("urls_new", templateVars);
+  }
 });
 
 app.post("/urls", (req, res) => {
-  const id = generateRandomString();
+  const user = req.cookies["user_id"];
   const longURL = req.body.longURL;
-  urlDatabase[id] = longURL;
+
+  if (!user) {
+    res.status(401).send("<html><body>You need to be logged in to shorten URLs.</body></html>");
+    return;
+  }
+
+  const id = generateRandomString();
+  urlDatabase[id].longURL = longURL;
   res.redirect(`/urls/${id}`);
 });
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  const longURL = urlDatabase[id].longURL;
   if (longURL) {
-    const templateVars = { id, longURL, user: req.cookies["username"] };
+    const templateVars = { id, longURL, user: req.cookies["user_id"] };
     res.render("urls_show", templateVars);
   } else {
     res.status(404).send("URL not found");
@@ -59,8 +77,8 @@ app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newLongURL = req.body.longURL;
 
-  if (urlDatabase[id]) {
-    urlDatabase[id] = newLongURL;
+  if (urlDatabase[id].longURL) {
+    urlDatabase[id].longURL = newLongURL;
     res.redirect("/urls");
   } else {
     res.status(404).send("URL not found");
@@ -69,28 +87,37 @@ app.post("/urls/:id", (req, res) => {
 
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  const longURL = urlDatabase[id].longURL;
   if (longURL) {
     res.redirect(longURL);
   } else {
-    res.status(404).send("URL not found");
+    res.status(404).send("<html><body>The URL you requested does not exist.</body></html>");
+  }
+});
+app.get("/login", (req, res) => {
+  const templateVars = { user: req.cookies["user_id"] };
+
+  if (templateVars.user) {
+    res.redirect("/urls");
+  } else {
+    res.render("login", templateVars);
   }
 });
 app.post('/login', (req, res) => {
-  const { username } = req.body;
-  res.cookie('username', username);
+  const { user_id } = req.body;
+  res.cookie('user_id', user_id);
   res.redirect('/urls');
 });
 app.post('/logout', (req, res) => {
-  res.clearCookie("username")
-  res.redirect('/urls')
+  res.clearCookie("user_id")
+  res.redirect('/login')
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
 
-  if (urlDatabase[id]) {
-    delete urlDatabase[id];
+  if (urlDatabase[id].longURL) {
+    delete urlDatabase[id].longURL;
     res.redirect("/urls");
   } else {
     res.status(404).send("URL not found");
@@ -101,8 +128,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get('/register', (req, res) => {
-  const templateVars = {}
-  res.render("register", templateVars);
+  const templateVars = {};
+
+  if (req.cookies["user_id"]) {
+    res.redirect("/urls");
+  } else {
+    res.render("register", templateVars);
+  }
 });
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
